cf2d28bb911862cd93894905a29e07b6d512db70
michelleyeung0 <35984950+michelleyeung0@users.noreply.github.com>
2026-05-12 19:56:54 -0700

    chore(pages): add boilerplate layout and page files

---
+++ src/app/shot-map/page.tsx
@@ -0,0 +1,7 @@
+export default function ShotMapPage() {
+  return (
+    <main>
+      <h1>Shot Map</h1>
+    </main>
+  );
+}
---
+++ src/app/tendencies/page.tsx
@@ -0,0 +1,7 @@
+export default function TendenciesPage() {
+  return (
+    <main>
+      <h1>Player Shot Tendencies</h1>
+    </main>
+  );
+}
